7a52c5f367916af074371c0c2f29d7e1980b8ce8
Blair Erickson <blair@jamwix.com>
2017-02-08 04:51:01 -0500

    working reveals

--- js/game.js
+++ js/game.js
@@ -6,23 +6,51 @@ $(document).ready(function ()
       hangarea.innerHTML = '<img src="http://tinyurl.com/j7vdmn4" width=30% >';  
 
      var words = ["dog","cat","rock","apple","banana","pond","frog","spider","zebra"];
+     var CurrentWord = words[Math.floor(Math.random() * words.length)];
+     var chr = 'd';
 
-     playarea2.innerHTML = '__________' 
+     document.getElementById("playarea3").innerHTML = 'Your current word is ' + CurrentWord + '<br>';
 
 
-function randompick() 
+// first draw
+     for (var i = 0; i < CurrentWord.length; i++) {
+       document.getElementById("playarea2").insertAdjacentHTML('beforeend', "  ___  ");   
+     }
+
+
+//redraw function that places the letters in case of a hit.
+
+function redraw()
+{
+     document.getElementById("playarea2").innerHTML = " ";
+     console.log("emptied" + chr);
+
+     for (var i = 0; i < CurrentWord.length; i++) 
+     {
+
+        if (CurrentWord[i].charAt(0) == chr)
             {
-            var x = Math.floor((Math.random() * words.length) + 1);
-            document.getElementById("playarea2").innerHTML = x + ' letters';
+                console.log("HIT " + chr);
+                document.getElementById("playarea2").insertAdjacentHTML('beforeend',  chr );   
             }
+        else 
+            {
+            document.getElementById("playarea2").insertAdjacentHTML('beforeend', "  ___  ");   
+             }
+     }
+ 
+}
 
 
+
+// looks for keystrokes and then sets them in lowercase before pasing them to the chr variable.
 
 $(document).keyup(event, function() 
         {
-     var chr = String.fromCharCode(event.keyCode);//converts the keycode into a character
+      chr = String.fromCharCode(event.keyCode);//converts the keycode into a character
         chr = chr.toLowerCase();
-        document.getElementById("playarea2").innerHTML = chr + 'test';
+        redraw();
+        console.log("redrawn..." + chr)
         });
 
 
